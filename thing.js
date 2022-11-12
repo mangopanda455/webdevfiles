b95058c9a6d360ac444a545cb866a3f4bab88b91
mangopanda455 <gfielding26@scarsdaleschools.org>
2022-11-11 21:24:43 -0500

    added some boxes for username and password with a function for the button click （っ＾▿＾）

---
+++ thing.js
@@ -0,0 +1,6 @@
+function show() {
+  document.getElementById('afterlogin').style.display = 'block';
+  document.getElementById('loginbuttonid').style.marginTop = 155;
+  document.getElementById('userid').value = "";
+  document.getElementById('passid').value = "";
+}
