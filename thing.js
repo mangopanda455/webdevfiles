ad21b8d62f22c0bc7df289bed6e452e669156d57
mangopanda455 <gfielding26@scarsdaleschools.org>
2022-11-12 13:54:41 -0500

    openable in jetbrains ides and changed margin on button push

--- thing.js
+++ thing.js
@@ -1,6 +1,6 @@
 function show() {
   document.getElementById('afterlogin').style.display = 'block';
-  document.getElementById('loginbuttonid').style.marginTop = 155;
+  document.getElementById('loginbuttonid').style.marginTop = 153.5;
   document.getElementById('userid').value = "";
   document.getElementById('passid').value = "";
 }
